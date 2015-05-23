f79fb952a6b2e3cce74c395b73dc1429124c2053
MichelDalia2012 <MichelDalia@MichelD2012sMBP.gateway>
2015-05-23 18:35:24 +1000

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by  MichelDalia2012 on 23/05/2015.
+//  Copyright (c) 2015 Michel Yousef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by  MichelDalia2012 on 23/05/2015.
+//  Copyright (c) 2015 Michel Yousef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
